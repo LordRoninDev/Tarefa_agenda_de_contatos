4532a468c8220a05cdea27f39d5d67e6c39b00af
Messias Garcia <Messiasmatos115@gmail.com>
2024-02-04 16:18:02 -0300

    adicionando arquivos do site de conatos

---
+++ main.js
@@ -0,0 +1,31 @@
+    const form = document.getElementById('form-contato');
+    
+    let linhas = '';
+    
+    form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value >= 0 ? 'Contato salvo' : 'Erro'}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
